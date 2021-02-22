c7a5b4f0ecdc7e80221f7078113a843c6c57de11
wancom <softpiware@gmail.com>
2021-02-23 01:00:24 +0900

    Implement timer

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,116 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Chart
+      :val="[timeoutTime - now, now - startTime]"
+      :width="100"
+      :height="100"
+    />
+    {{ Math.floor((timeoutTime - now) / 1000 / 60) }}:{{
+      Math.floor(((timeoutTime - now) / 1000) % 60)
+    }}<br />
+    <input type="number" v-model="timemin" min="0" max="59" />:
+    <input type="number" v-model="timesec" min="0" max="59" />
+    <button
+      @click="startTimer"
+      v-if="this.timerStatus == 0 || this.timerStatus == 1"
+    >
+      Start
+    </button>
+    <button @click="stopTimer" v-if="this.timerStatus == 2">Stop</button>
+    <button @click="resetTimer">Reset</button>
+    <audio
+      ref="entersound"
+      src="finish.mp3"
+      preload="auto"
+      :muted="false"
+    ></audio>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Chart from "./components/Chart.vue";
+
+enum TimerStatus {
+  Stop = 0,
+  Pause = 1,
+  Running = 2
+}
+
+const tickInterval = 100;
 
 @Component({
   components: {
-    HelloWorld
+    Chart
   }
 })
-export default class App extends Vue {}
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+
+  private timerStatus = TimerStatus.Stop;
+
+  private timerID = -1;
+  private timesec = 0;
+  private timemin = 1;
+  private startTime = 0;
+  private timeoutTime = 1;
+  private now = 0;
+  public startTimer() {
+    if (this.timerID != -1) {
+      clearInterval(this.timerID);
+      this.timerID = -1;
+    }
+    if (this.timerStatus == TimerStatus.Stop) {
+      this.playSound(true);
+      const time = this.timesec * 1 + this.timemin * 60;
+      this.now = new Date().getTime();
+      this.startTime = this.now;
+      this.timeoutTime = this.now + time * 1000;
+    } else {
+      const oldnow = this.now;
+      this.now = new Date().getTime();
+      this.startTime += this.now - oldnow;
+      this.timeoutTime += this.now - oldnow;
+    }
+    this.timerStatus = TimerStatus.Running;
+    this.timerID = setInterval(() => {
+      this.tick();
+    }, tickInterval);
+  }
+  public stopTimer() {
+    if (this.timerID != -1) {
+      clearInterval(this.timerID);
+      this.timerID = -1;
+    }
+    this.timerStatus = TimerStatus.Pause;
+  }
+  public resetTimer() {
+    this.stopTimer();
+    this.timerStatus = TimerStatus.Stop;
+    this.now = 0;
+    this.startTime = 0;
+    this.timeoutTime = 1;
+  }
+  private playSound(mute: boolean) {
+    // eslint-disable-next-line
+    const aud = this.$refs["entersound"] as any;
+    if (typeof aud.currentTime != "undefined") {
+      aud.currentTime = 0;
+    }
+    aud.muted = mute;
+    return aud.play();
+  }
+  private tick() {
+    this.now = new Date().getTime();
+    if (this.timeoutTime <= this.now) {
+      this.resetTimer();
+      this.playSound(false).then(() => {
+        alert("終了！");
+      });
+      return;
+    }
+  }
+}
 </script>
 
 <style>
@@ -23,7 +119,5 @@ export default class App extends Vue {}
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Chart.vue
@@ -0,0 +1,42 @@
+<script>
+import { Pie } from "vue-chartjs";
+
+export default {
+  extends: Pie,
+  name: "chart",
+  props: ["val"],
+  data() {
+    return {
+      data: {
+        datasets: [
+          {
+            data: this.val,
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)"
+            ],
+            borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        animation: false,
+        // responsive: false,
+        tooltips: {
+          enabled: false //ここにfalseを指定
+        }
+      }
+    };
+  },
+  mounted() {
+    this.renderChart(this.data, this.options);
+  },
+  watch: {
+    val() {
+      this.data.datasets[0].data = this.val;
+      this.renderChart(this.data, this.options);
+    }
+  }
+};
+</script>
